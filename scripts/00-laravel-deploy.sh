cac7b502b282b5557f2220e381c6e3248fc10f4c
CosmosViajes <cosmosviajes23@gmail.com>
2025-05-03 14:05:25 +0200

    Configuración Docker para Render

---
+++ scripts/00-laravel-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Instalando dependencias..."
+composer install --no-dev --optimize-autoloader
+
+echo "Optimizando Laravel..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+echo "Ejecutando migraciones..."
+php artisan migrate --force
